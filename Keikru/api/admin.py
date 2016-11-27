735171a829d2f4377923c85cff875cc26b5e194c
hafiz703 <mhbt-703@hotmail.com>
2016-11-27 17:41:38 +0800

    Revert "Keikru Django files"
    This reverts commit 18851c3764cd348fafb7f03004112ea864ec9f81.

--- Keikru/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Artist,Label,Album,Song
-# Register your models here.
-
-admin.site.register(Label)
-admin.site.register(Artist)
-admin.site.register(Album)
-admin.site.register(Song)
-
---
+++ sqlQueries.sql
@@ -0,0 +1,79 @@
+
+
+--Drop Schema dbproj;
+--Create Schema dbproj;
+use dbproj;
+Create Table Users(
+  user_id int NOT NULL AUTO_INCREMENT,
+  username char(50) NOT NULL UNIQUE,
+  password char(20 NOT NULL,
+  Primary Key (user_id)
+);
+
+
+
+Create table Record_Label(
+  label_id int,
+  label_name char(20)NOT NULL,
+  Primary Key (label_id)
+);
+
+Create table Artiste(
+  artiste_id int,
+  artiste_name char(50) NOT NULL,
+  managed_by int  NOT NULL,
+
+  Primary Key (artiste_id),
+  Foreign Key (managed_by) References Record_Label(label_id)
+
+) ;
+
+Create table Album(
+  album_id int,
+  album_name char(20),
+  genre char(20),
+  song_avg_rating int,
+  contributing_artiste int  NOT NULL,
+
+  Primary Key (album_id),
+  Foreign Key (contributing_artiste) References Artiste(artiste_id)
+);
+
+Create table Song(
+  song_id int,
+  song_name char(20),
+  avg_rating int,
+  play_count int,
+  album_id int NOT NULL,
+
+  Primary Key (song_id),
+  Foreign Key (album_id) REFERENCES Album(album_id)
+
+);
+
+  Create table listen_record(
+  	user_id int,
+  	listen_count int,
+  	song_id int,
+  
+
+  	Primary Key (user_id , song_id),
+ 	Foreign Key (user_id) References Users(user_id),
+  	Foreign Key (song_id) References Song(song_id)
+
+
+
+
+);
+
+Create table user_rating(
+	user id int,
+	song_id int,
+
+Primary Key (user_id , song_id),
+ 	Foreign Key (user_id) References Users(user_id),
+  	Foreign Key (song_id) References Song(song_id)
+
+	
+);
+
