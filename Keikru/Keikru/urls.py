75861ff78bd4746a2a672c7565bdffcafb9b1b45
hafiz703 <mhbt-703@hotmail.com>
2016-12-11 21:02:26 +0800

    fixed audio shit

--- Keikru/Keikru/urls.py
+++ Keikru/Keikru/urls.py
@@ -26,3 +26,7 @@ urlpatterns = [
     url(r'^music/', include('music.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
