47d43748c1e78579c737304e17e74aeaa890039a
ndmnh <ndminh208@gmail.com>
2016-12-11 14:13:55 +0800

    new recommendation engine, works

--- Keikru/music/static/music/app.js
+++ Keikru/music/static/music/app.js
@@ -148,6 +148,93 @@ myApp.controller("SongController", ['$scope','$http', function($scope,$http) {
     });
   };
 
+  // $scope.setRecommendedPlaylist = function() {
+  //   var link = 'http://127.0.0.1:8000/api/user-song-rating/?format=json';
+  //   $.ajax({
+  //     'type': 'GET',
+  //     'url': link, //updating song with song_id = 2
+  //     'contentType': 'application/json',
+  //     'dataType': 'json',
+  //     'success': function(data) {
+  //       $scope.currentPlaylist.name = "Recommended Playlist";
+  //       similarity = [];
+  //       $scope.rated_song_IDs = [];
+  //       rated_song_IDs_sim = [];
+  //       for (usr in data) {
+  //         if (data[usr].user!=$scope.NgUserID) {
+  //           existing = false;
+  //           for (i in similarity) {
+  //             if (similarity[i].id==data[usr].user) {
+  //               existing = true;
+  //               break;
+  //             }
+  //           }
+  //           if (!existing) {
+  //             usr_diff = {
+  //               id: data[usr].user,
+  //               songsInCommon: [],
+  //               similarityLevel: 0
+  //             };
+  //             similarity.push(usr_diff);
+  //           }
+  //         } else {
+  //           $scope.rated_song_IDs.push(data[usr].song_rated);
+  //         }
+  //       }
+  //       // console.log($scope.rated_song_IDs);
+  //       for (usr in data) {
+  //         if (data[usr].user==$scope.NgUserID) {
+  //           for (usr_diff in data) {
+  //             if (data[usr_diff].user!=$scope.NgUserID) {
+  //               if (data[usr_diff].song_rated==data[usr].song_rated) {
+  //                 for (stat in similarity) {
+  //                   if (similarity[stat].id==data[usr_diff].user) {
+  //                     similarity[stat].songsInCommon.push(data[usr_diff].song_rated);
+  //                     similarity[stat].similarityLevel += Math.pow(data[usr_diff].rating-data[usr].rating,2);
+  //                   }
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //       // the lower the similarity the better
+  //       // the bigger the songsInCommon the better
+  //       // -> the lower the fraction the better
+  //       // console.log(similarity);
+  //       most_similar_usr = similarity[0];
+  //       for (usr_diff in similarity) {
+  //         if (similarity[usr_diff].songsInCommon.length!=0) {
+  //           if ((similarity[usr_diff].similarity/similarity[usr_diff].songsInCommon.length)<(most_similar_usr.similarity/most_similar_usr.songsInCommon.length)) {
+  //             most_similar_usr = similarity[usr_diff];
+  //           }
+  //         }
+  //       }
+  //       // console.log(most_similar_usr);
+  //       recommended_song_IDs = [];
+  //       for (usr in data) {
+  //         if (data[usr].user==most_similar_usr.id) {
+  //           rated_song_IDs_sim.push(data[usr].song_rated);
+  //         }
+  //       }
+
+  //       for (song in rated_song_IDs_sim) {
+  //         if (!$scope.rated_song_IDs.includes(rated_song_IDs_sim[song])) {
+  //           recommended_song_IDs.push(rated_song_IDs_sim[song]);
+  //         }
+  //       }
+
+  //       // console.log(recommended_song_IDs);
+  //       for (i in $scope.allSongs) {
+  //         if (recommended_song_IDs.includes($scope.allSongs[i].id)) {
+  //           $scope.currentPlaylist.songList.push($scope.allSongs[i]);
+  //         }
+  //       }
+  //       // console.log($scope.currentPlaylist.songList);
+  //       $scope.changePage('Playlist');
+  //     }
+  //   });
+  // };
   $scope.setRecommendedPlaylist = function() {
     var link = 'http://127.0.0.1:8000/api/user-song-rating/?format=json';
     $.ajax({
@@ -178,7 +265,11 @@ myApp.controller("SongController", ['$scope','$http', function($scope,$http) {
               similarity.push(usr_diff);
             }
           } else {
-            $scope.rated_song_IDs.push(data[usr].song_rated);
+            song = {
+              id: data[usr].song_rated,
+              rating: data[usr].rating
+            }
+            $scope.rated_song_IDs.push(song);
           }
         }
         // console.log($scope.rated_song_IDs);
@@ -217,9 +308,15 @@ myApp.controller("SongController", ['$scope','$http', function($scope,$http) {
             rated_song_IDs_sim.push(data[usr].song_rated);
           }
         }
-
+        // console.log(rated_song_IDs_sim);
         for (song in rated_song_IDs_sim) {
-          if (!$scope.rated_song_IDs.includes(rated_song_IDs_sim[song])) {
+          rated = false;
+          for (song_rated in $scope.rated_song_IDs) {
+            if ($scope.rated_song_IDs[song_rated].id == rated_song_IDs_sim[song]) {
+              rated = true;
+            }
+          }
+          if (!rated) {
             recommended_song_IDs.push(rated_song_IDs_sim[song]);
           }
         }
@@ -235,7 +332,6 @@ myApp.controller("SongController", ['$scope','$http', function($scope,$http) {
       }
     });
   };
-
   $scope.setPlaylistByAlbum = function(album_id) {
     var link = 'http://127.0.0.1:8000/api/album/'+album_id+'/?format=json';
     $.ajax({
